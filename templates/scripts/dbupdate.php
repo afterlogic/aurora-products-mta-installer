41ef4b489a0283850a923a9e6478bcdb432f7342
Afterlogic Support <support@afterlogic.com>
2023-06-30 14:46:34 +0300

    Improved error handling in dbupdate.php script

--- templates/scripts/dbupdate.php
+++ templates/scripts/dbupdate.php
@@ -16,18 +16,32 @@ $oServer->save();
 $oSettings = \Aurora\System\Api::GetSettings();
 $oLicensingModule = \Aurora\Modules\LicensingWebClient\Module::getInstance();
 $sLinkTrial = $oLicensingModule->oModuleSettings->TrialKeyLink;
-$aMatch=array();
-preg_match('/href=["\']?([^"\'>]+)["\']?/', $sLinkTrial, $aMatch);
-$sUrlTrial = $aMatch[1]."&format=json";
-$sKeyTrialJson = get_data($sUrlTrial);
-$oResponse = json_decode($sKeyTrialJson);
-if (isset($oResponse->success) && $oResponse->success && isset($oResponse->key) && $oResponse->key !== '') {
-    $oSettings->LicenseKey = $oResponse->key;
-    $oSettings->Save();
+if ($sLinkTrial) {
+    $aMatch = array();
+    preg_match('/href=["\']?([^"\'>]+)["\']?/', $sLinkTrial, $aMatch);
+    if (isset($aMatch[1]) && filter_var($aMatch[1], FILTER_VALIDATE_URL) !== false) {
+        $aUrlParts = parse_url($aMatch[1]);
+        if (isset($aUrlParts['query'])) {
+            parse_str($aUrlParts['query'], $aQueryParams);
+        } else {
+            $aQueryParams = array();
+        }
+        $aQueryParams["format"] = "json";
+        $sUrlTrial = $aUrlParts['scheme'] . '://' . $aUrlParts['host'] . $aUrlParts['path'] . '?' . http_build_query($aQueryParams);
+        $sKeyTrialJson = get_data($sUrlTrial);
+        $oResponse = json_decode($sKeyTrialJson);
+        if (isset($oResponse->success) && $oResponse->success && isset($oResponse->key) && $oResponse->key !== '') {
+            $oSettings->LicenseKey = $oResponse->key;
+            $oSettings->Save();
+        }
+    }
 }
 
 function get_data($url)
 {
+    if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+        return false;
+    }
     $ch = curl_init();
     $timeout = 20;
     curl_setopt($ch, CURLOPT_URL, $url);
